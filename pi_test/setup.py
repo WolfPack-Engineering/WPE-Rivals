df85a1b497f45366d2343ee2eaeb805692c24db1
Fishflopper <seth.eno@coronastreet.net>
2026-02-22 11:45:24 -0800

    Fixed Configs
    Aha! I hath discovered the pkg create command which makes all my configuration files for me!

--- pi_test/setup.py
+++ pi_test/setup.py
@@ -1,20 +1,31 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 package_name = 'pi_test'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='seno',
+    maintainer_email='seno@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
     entry_points={
         'console_scripts': [
-            'pi_test_listener = pi_test.pi_test_listener:main',
             'pi_test_sender = pi_test.pi_test_sender:main',
+            'pi_test_listener = pi_test.pi_test_listener:main'
         ],
     },
 )
--- pytest_build_file/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/seno/ros2_ws/install/pi_test'
